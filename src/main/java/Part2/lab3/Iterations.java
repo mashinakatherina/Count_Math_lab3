00ae74fbdacd8802b7fa47b6d7ccf1410bb8655d
Katherina <mashina.katherina@gmail.com>
2020-04-06 19:21:59 +0300

    final

--- src/main/java/Part2/lab3/Iterations.java
+++ src/main/java/Part2/lab3/Iterations.java
@@ -2,10 +2,12 @@ package Part2.lab3;
 
 public class Iterations {
     public static final double EPS = 0.0001;
+    public static final int MAX_NUMBER_OF_ITERATIONS = 100000;
     public static double y0 = 0;
     public static double x0 = 0;
     public static double countX(Function f1, Function f2) {
-        double d1 = 0, d2 = 0, x = 0, y = 0;
+        int k = 0;
+        double d1, d2, x, y;
         do
         {
             x = f1.signifyX(y0);
@@ -14,7 +16,8 @@ public class Iterations {
             d2 = f2.equateToZero(x, y);
             x0 = x;
             y0 = y;
-        } while (Math.abs(d1) > EPS && Math.abs(d2)> EPS);
+            k++;
+        } while (Math.abs(d1) > EPS && Math.abs(d2)> EPS || k < MAX_NUMBER_OF_ITERATIONS);
         x0 = 0;
         y0 = 0;
         return x;
