081891331663c05e4f2459c23e100ee04f3260e7
Katherina <mashina.katherina@gmail.com>
2020-04-07 11:55:49 +0300

    final

--- src/main/java/Part2/lab3/Iterations.java
+++ src/main/java/Part2/lab3/Iterations.java
@@ -8,33 +8,35 @@ public class Iterations {
     
     public static double countX(Function f1, Function f2) {
         int k = 0;
-        double d1, d2, x, y;
+        double s1, s2, x, y;
         do
         {
             x = f1.signifyX(y0);
             y = f2.getValue(x0);
-            d1 = f1.equateToZero(x, y);
-            d2 = f2.equateToZero(x, y);
+            s1 = f1.equateToZero(x, y);
+            s2 = f2.equateToZero(x, y);
             x0 = x;
             y0 = y;
             k++;
-        } while (Math.abs(d1) > EPS && Math.abs(d2)> EPS || k < MAX_NUMBER_OF_ITERATIONS);
+        } while (Math.abs(s1) > EPS && Math.abs(s2)> EPS || k < MAX_NUMBER_OF_ITERATIONS);
         x0 = 0;
         y0 = 0;
         return x;
     }
 
     public static double countY(Function f1, Function f2) {
-        double d1, d2, y;
+        double s1, s2, y;
+        int k =0;
         do
         {
             double x = f1.signifyX(y0);
             y = f2.getValue(x0);
-            d1 = f1.equateToZero(x, y);
-            d2 = f2.equateToZero(x, y);
+            s1 = f1.equateToZero(x, y);
+            s2 = f2.equateToZero(x, y);
             x0 = x;
             y0 = y;
-        } while (Math.abs(d1) > EPS && Math.abs(d2)> EPS);
+            k ++;
+        } while (Math.abs(s1) > EPS && Math.abs(s2)> EPS || k < MAX_NUMBER_OF_ITERATIONS);
         x0 = 0;
         y0 = 0;
         return y;
